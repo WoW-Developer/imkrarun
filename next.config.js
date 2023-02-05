f3ca72cf423859061704af0f6dafe47e809884db
Kr Arun <wowdeveloper@protonmail.com>
2023-02-05 12:49:57 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
