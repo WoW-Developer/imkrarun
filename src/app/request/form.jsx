b4052475e80b3ee31ecc227c5b625b45b5656106
WoW Developer <wowdeveloper@prtonmail.com>
2023-02-06 06:27:56 +0530

    tryed

--- src/app/request/form.jsx
+++ src/app/request/form.jsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { useState } from "react";
+import { sendEtagResponse } from "next/dist/server/send-payload";
+import React, { useEffect, useState } from "react";
 
 const Form = () => {
   const [formData, setFormData] = useState({
@@ -9,6 +10,24 @@ const Form = () => {
     suggestion: "",
   });
 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    const spin = document?.getElementById("spinner");
+    const frm = document?.getElementById("form");
+    if (loading) {
+      spin.style.display = "block";
+      frm.style.display = "none";
+    } else {
+      frm.style.display = "block";
+      spin.style.display = "none";
+    }
+  }, [loading]);
+
   const handleInputChange = (event) => {
     setFormData({
       ...formData,
@@ -17,6 +36,7 @@ const Form = () => {
   };
 
   const handleSubmit = async (event) => {
+    setLoading(true);
     event.preventDefault();
     const name = document.getElementById("name");
     const errname = document.getElementById("errname");
@@ -29,16 +49,20 @@ const Form = () => {
 
     if (formData.name == null || formData.name.length == 0) {
       errname.style({ display: "block" });
+      setLoading(false);
       return;
     }
     errname.style({ display: "none" });
 
     if (formData.mail == null || formData.email.length == 0) {
       errmail.style({ display: "block" });
+      setLoading(false);
+      return;
     }
     errmail.style({ display: "none" });
     if (formData.phone == null || formData.phone.length == 0) {
       errphone.style({ display: "block" });
+      return;
     }
     errphone.style({ display: "none" });
 
@@ -46,9 +70,30 @@ const Form = () => {
   };
 
   return (
-    <div className="p-4 m-8">
+    <div className="p-4 m-8 flex flex-grow items-center justify-center">
+      <div id="spinner" role="status">
+        <svg
+          aria-hidden="true"
+          className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+        <span className="sr-only">Loading...</span>
+      </div>
       <form
-        className="bg-white p-6 rounded-lg shadow-md"
+        id="form"
+        className="bg-white grow hidden p-6 mb-14 mx-4 
+        rounded-lg shadow-inner"
         onSubmit={handleSubmit}
       >
         <div className="mb-4">
@@ -57,7 +102,7 @@ const Form = () => {
           </label>
           <input
             autoComplete="off"
-            className="w-full focus:outline focus:outline-blue-500 form-input border rounded border-gray-300 p-2"
+            className="w-full starlabel focus:outline focus:outline-blue-500 form-input border rounded border-gray-300 p-2"
             type="text"
             id="name"
             name="name"
@@ -73,7 +118,7 @@ const Form = () => {
             Email
           </label>
           <input
-            className="w-full form-input border rounded focus:outline focus:outline-blue-500   border-gray-300 p-2"
+            className="w-full starlabel  form-input border rounded focus:outline focus:outline-blue-500   border-gray-300 p-2"
             type="email"
             autoComplete="off"
             id="email"
@@ -90,7 +135,7 @@ const Form = () => {
             Phone
           </label>
           <input
-            className="w-full border form-input rounded focus:outline focus:outline-blue-500  border-gray-300 p-2"
+            className="w-full starlabel border form-input rounded focus:outline focus:outline-blue-500  border-gray-300 p-2"
             type="tel"
             id="phone"
             autoComplete="off"
