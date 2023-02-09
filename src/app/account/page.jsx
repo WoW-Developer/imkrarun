c9056c23d31b322978545a59ebc9004718ef3e81
Kr Arun <wowdeveloper@protonmail.com>
2023-02-09 02:56:21 +0000

    ss

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='w-full justify-center items-center pt-14'>
-        <div className="flex m-auto flex-col text-center justify-center mx-auto w-full max-w-[600px]">
+    <div className='w-full flex flex-col justify-center md:pt-14'>
+        <div className="h-screen flex flex-col items-center justify-center mx-auto max-w-[600px]">
         <h1>I am Login</h1>
         <h1>I am Login</h1>
 
