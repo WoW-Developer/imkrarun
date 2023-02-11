fe0b70a0927f7841f897795cd3c9240ba21ff042
Kr Arun <wowdeveloper@protonmail.com>
2023-02-11 15:58:17 +0530

    edited text of root page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -38,9 +38,9 @@ export default function Home() {
         {/* End of 2 Cols */}
       </div>
       <h1 className="text-white whitespace-pre-wrap select-none text-xl p-2 mt-28">
-        Dear Students / Parents, {"\n"} Are you or your child struggling with
+        Dear Students / Parents,{"\n"}Are you or your child struggling with
         mathematics and in need of a tutor who can help improve their
-        understanding and confidence in the subject? If so, I am here to help! I
+        understanding and confidence in the subject?{"\n"}If so, I am here to help! I
         am a highly experienced and qualified mathematics tutor with a passion
         for helping students achieve their full potential. I have a deep
         understanding of mathematical concepts and am able to explain them in a
@@ -48,13 +48,13 @@ export default function Home() {
         retain the information. I also believe in creating a positive and
         supportive learning environment that encourages students to ask
         questions and engage with the material. My approach to tutoring is
-        tailored to meet the needs of each individual student. I start by
+        tailored to meet the needs of each individual student.{"\n"}I start by
         conducting a thorough assessment of the student&#039;s current knowledge
         and skills, and then create a customized learning plan that is designed
         to help them achieve their goals. Whether you or your child is
         struggling with a specific math topic, preparing for an exam, or just
         looking to improve their overall understanding and confidence, I am here
-        to help. So why wait? If you&#039;re ready to take the next step in
+        to help.{"\n"}So why wait? If you&#039;re ready to take the next step in
         improving your or your child&#039;s math skills, contact me today to
         schedule your first session. I look forward to helping you achieve your
         academic and personal goals!
